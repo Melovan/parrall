28d0c14fbec5fe870e42f0316b7367981441723b
Melovan <mrdanhik@gmail.com>
2022-02-10 03:54:04 +0000

    float

---
+++ Source1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <malloc.h>
+#define N  10000000
+#define M_PI		3.14159265358979323846
+
+int main()
+{
+
+	float* sins = (float*)calloc(N, sizeof(float));
+	for (size_t i = 0; i < N; ++i)
+		sins[i] = sin((2 * M_PI) / N * i);
+	float summ = 0;
+#pragma data copyin(sins) copyout(summ)
+	{
+#pragma acc kernels
+		{
+
+			for (size_t i = 0; i < N; ++i)
+				summ = summ + sins[i];
+
+		}
+	}
+	printf("%e", summ);
+	return 0;
+}
